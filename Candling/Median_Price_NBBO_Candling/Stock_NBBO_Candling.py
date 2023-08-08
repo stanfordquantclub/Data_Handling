b51a22048d25fd7a27f46a623429e972056d7aaf
lukepark2004 <47678247+lukepark2004@users.noreply.github.com>
2023-08-08 02:11:54 -0700

    Finished report detailing current progress

--- Candling/Median_Price_NBBO_Candling/Stock_NBBO_Candling.py
+++ Candling/Median_Price_NBBO_Candling/Stock_NBBO_Candling.py
@@ -1,28 +1,13 @@
-# %%
-import sys
-sys.path.append("/home/luke/Documents/GitHub/Data_Handling")
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from Training_Features.realized_volatiliity import realized_vol
+from datetime import date, time, datetime, timedelta
+
 
 # %%
-path = "/srv/sqc/data/client-2378-luke-eq-taq/2022/20220401/S/SPY.csv.gz"
+path = "/srv/sqc/data/client-2378-luke-eq-taq/2022/20221201/S/SPY.csv.gz"
 file = pd.read_csv(path)
 
 # %%
-candled_path = "/srv/sqc/volatility_exploration/NBBO_Candled/Most_Recent_Protocol/Stock_Data/20221215.csv"
-candled_file = pd.read_csv(candled_path)
-ask_list = candled_file['Ask'].tolist()
-plt.plot(ask_list)
-plt.show()
-# %%
-
-
-real_vol = realized_vol(ask_list, 600)
-plt.plot(real_vol[:-600])
-plt.show()
-
-
-# %%
+start_time=time(9, 30, 0)
+end_time=time(16, 0, 0)
