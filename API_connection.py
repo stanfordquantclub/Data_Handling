3c68d2a22d87d0f8d88f40a01d3cee2190ece14c
lukepark2004 <lukepark2004@gmail.com>
2023-12-25 13:00:57 -0800

    working on api stuff

--- API_connection.py
+++ API_connection.py
@@ -1,19 +1,56 @@
+# %%
 from ib_insync import *
-import datetime
-# util.startLoop()  # uncomment this line when in a notebook
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
+import pandas_market_calendars as mcal
 
+# %%
 ib = IB()
-ib.connect('127.0.0.1', 7496, clientId=1)
 
-earlier = datetime.datetime(2023, 11, 21, 7, 30, 0)
+# %%
+print(ib.accountSummary())
+# %%
+# getting all market days from January 2021, to November 2023
+nyse = mcal.get_calendar('NYSE')
+open_times = (nyse.schedule(start_date = '2021-01-01', end_date = '2023-11-28', tz = 'EST'))['market_open'].to_list()
+close_times = (nyse.schedule(start_date = '2021-01-01', end_date = '2023-11-28', tz = 'EST'))['market_close'].to_list()
+
+
+# %%
+def retrieve(contract, endDateTime, durationStr, barSizeSetting, whatToShow):
+    bars = ib.reqHistoricalData(contract, endDateTime=endDateTime,
+                                durationStr=durationStr, barSizeSetting=barSizeSetting,
+                                whatToShow=whatToShow, useRTH=True)
+    
+    return bars
+
+# %%
+
+def daysDataType(open_time, close_time, contract, whatToShow):
+    cur_time = open_time + timedelta(minutes = 30)
+    daysDf = util.df(retrieve(contract = contract, endDateTime=cur_time, durationStr='1800 S', 
+               barSizeSetting='1 secs', whatToShow=whatToShow))
+
+    while(cur_time != close_time):
+        cur_time += timedelta(minutes = 30)
+        daysDf = pd.concat([daysDf, 
+                            util.df(retrieve(contract = contract, 
+                                             endDateTime=cur_time, durationStr='1800 S', 
+                                             barSizeSetting='1 secs', whatToShow=whatToShow))])
+        
+        daysDf.rename(columns = {"date": "date",
+                                 "open": whatToShow + "_open",
+                                 "high": whatToShow + "_high",
+                                 "low": whatToShow + "_low",
+                                 "close": whatToShow + "_close"})
+    
+    return daysDf
 
 contract = Stock('SPY', 'SMART', 'USD')
-bars = ib.reqHistoricalData(
-    contract, endDateTime= earlier, durationStr='1800 S',
-    barSizeSetting='1 secs', whatToShow='TRADES', useRTH=True)
+newDf = daysDataType(open_times[0], close_times[0], contract, "ASK")
 
-# convert to pandas dataframe (pandas needs to be installed):
-df = util.df(bars)
-print(df)
+# %%
+print(newDf)
 
-print("hello world")
+# %%
